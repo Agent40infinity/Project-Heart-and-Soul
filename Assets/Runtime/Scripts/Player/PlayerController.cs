40efed1caf1651afd1185a07b49ea6b1f3c50bd6
Aiden Nathan <agent9@y7mail.com>
2024-08-26 12:58:57 +1000

    Folder Reorganisation

---
+++ Assets/Runtime/Scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Dictionary<KeyCode, Vector2> keyDict = new Dictionary<KeyCode, Vector2>()
+        {
+            { KeyCode.W, new Vector2(0, 1) },
+            { KeyCode.A, new Vector2(-1, 0) },
+            { KeyCode.S, new Vector2(0, -1) },
+            { KeyCode.D, new Vector2(1, 1) },
+        };
+        
+        private KeyCode lastKey;
+
+        private Rigidbody rigid;
+
+        public void Awake()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        public void Update()
+        {
+            GetInput();
+            Physics();
+        }
+
+        public void GetInput()
+        {
+            if (Input.anyKeyDown)
+            {
+                var current = Event.current.keyCode;
+
+                if (!keyDict.ContainsKey(current))
+                {
+                    return;
+                }
+
+                lastKey = Event.current.keyCode;
+            }
+        }
+
+        public void Physics()
+        { 
+           
+        }
+    }
+}
