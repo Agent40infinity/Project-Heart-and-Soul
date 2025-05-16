7bf1266f4990a647ac49fb30f83be8fe02c0881c
Aiden Nathan <agent9@y7mail.com>
2025-05-17 00:29:04 +1000

    Maybe added Databind?

---
+++ Packages/Slash.Unity.DataBind/Runtime/Scripts/UI/Unity/Observers/ToggleValueObserver.cs
@@ -0,0 +1,34 @@
+﻿namespace Slash.Unity.DataBind.UI.Unity.Observers
+{
+    using Slash.Unity.DataBind.Foundation.Observers;
+    using UnityEngine.UI;
+
+    /// <summary>
+    ///   Observer for the value of a <see cref="Toggle"/>.
+    /// </summary>
+    public class ToggleValueObserver : ComponentDataObserver<Toggle, bool>
+    {
+        /// <inheritdoc />
+        protected override void AddListener(Toggle target)
+        {
+            target.onValueChanged.AddListener(this.OnToggleValueChanged);
+        }
+
+        /// <inheritdoc />
+        protected override bool GetValue(Toggle target)
+        {
+            return target.isOn;
+        }
+
+        /// <inheritdoc />
+        protected override void RemoveListener(Toggle target)
+        {
+            target.onValueChanged.RemoveListener(this.OnToggleValueChanged);
+        }
+
+        private void OnToggleValueChanged(bool newValue)
+        {
+            this.OnTargetValueChanged();
+        }
+    }
+}
