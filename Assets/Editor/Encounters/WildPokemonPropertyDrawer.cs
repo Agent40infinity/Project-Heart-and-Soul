b718268db5bc3d7c5cf7f18ec81801a21fd6b19b
Aiden Nathan <agent9@y7mail.com>
2025-05-18 01:42:58 +1000

    MODIFIED - EncounterTable Drawers

--- Assets/Editor/Encounters/WildPokemonPropertyDrawer.cs
+++ Assets/Editor/Encounters/WildPokemonPropertyDrawer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
@@ -18,11 +16,12 @@ public class WildPokemonPropertyDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         var pokemonProperty = property.FindPropertyRelative("pokemon");
-        var minLevelProperty = property.FindPropertyRelative("minLvl");
-        var maxLevelProperty = property.FindPropertyRelative("maxLvl");
+        var levelProperty = property.FindPropertyRelative("level");
         var rateProperty = property.FindPropertyRelative("rate");
 
         EditorGUI.BeginProperty(position, label, property);
+
+        var origin = position;
 
         position.y += EditorGUIUtility.singleLineHeight;
         position.x += buffer;
@@ -30,31 +29,33 @@ public class WildPokemonPropertyDrawer : PropertyDrawer
         var originWidth = position.width;
 
         // Left Column
-        position.width = encounterHeight;
+        position.width = encounterHeight * 4;
+
+        var background = new GUIStyle("Window").normal.scaledBackgrounds[0];
+        var backgroundRect = new Rect(
+            position.x - EditorGUIUtility.singleLineHeight / 2f,
+            position.y - EditorGUIUtility.singleLineHeight / 3f,
+            encounterHeight,
+            encounterHeight);
+
+        GUI.DrawTexture(backgroundRect, background);
+
         var pokeBase = pokemonProperty.boxedValue as PokemonBase;
 
         if (pokeBase != null)
         {
-            var sprite = GetTextureFromSprite(pokeBase.pokedexSprite);
-
-            EditorGUI.LabelField(position, new GUIContent(sprite));
-        }
-        else
-        {
-            EditorGUI.LabelField(position, new GUIContent("Placeholder"));
+            EditorGUI.PrefixLabel(origin, string.IsNullOrEmpty(pokeBase.name) ? label : new GUIContent(pokeBase.name));
+            GetTextureFromSprite(pokeBase.pokedexSprite, position);
         }
 
         // Right Column
         position.x += encounterHeight;
         position.width = originWidth - encounterHeight;
-
+        EditorGUIUtility.labelWidth = originWidth / 8;
         EditorGUI.PropertyField(position, pokemonProperty, true);
 
         position.y += EditorGUIUtility.singleLineHeight;
-        EditorGUI.PropertyField(position, minLevelProperty, true);
-
-        position.y += EditorGUIUtility.singleLineHeight;
-        EditorGUI.PropertyField(position, maxLevelProperty, true);
+        EditorGUI.PropertyField(position, levelProperty, true);
 
         position.y += EditorGUIUtility.singleLineHeight;
         EditorGUI.PropertyField(position, rateProperty, true);
@@ -62,23 +63,34 @@ public class WildPokemonPropertyDrawer : PropertyDrawer
         EditorGUI.EndProperty();
     }
 
-    private Texture GetTextureFromSprite(Sprite sprite)
+    private void GetTextureFromSprite(Sprite sprite, Rect position)
     {
         if (sprite == null)
         {
-            return null;
+            return;
         }
 
-        var croppedTexture = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+        var tex = sprite.texture;
+        var spriteRect = sprite.textureRect;
 
-        var pixels = sprite.texture.GetPixels((int)sprite.textureRect.x,
-                                                (int)sprite.textureRect.y,
-                                                (int)sprite.textureRect.width,
-                                                (int)sprite.textureRect.height);
+        var aspectWidth = spriteRect.width / spriteRect.height;
+        var aspectHeight = spriteRect.height / spriteRect.width;
 
-        croppedTexture.SetPixels(pixels);
-        croppedTexture.Apply();
+        var uv = new Rect(
+            spriteRect.x / tex.width,
+            spriteRect.y / tex.height,
+            spriteRect.width / tex.width,
+            spriteRect.height / tex.height);
 
-        return croppedTexture;
+        var minPos = EditorGUIUtility.singleLineHeight / 2;
+        var maxDiameter = EditorGUIUtility.singleLineHeight * 3;
+
+        var previewRect = new Rect(
+            position.x + minPos + (aspectWidth > 1 ? 0 : (maxDiameter - (maxDiameter * aspectWidth)) / 2),
+            position.y + minPos + (aspectHeight > 1 ? 0 : (maxDiameter - (maxDiameter * aspectHeight)) / 2),
+            maxDiameter * (aspectWidth > 1 ? 1 : aspectWidth),
+            maxDiameter * (aspectHeight > 1 ? 1 : aspectHeight));
+
+        GUI.DrawTextureWithTexCoords(previewRect, tex, uv, true);
     }
 }
--- Assets/Runtime/Scripts/Engine/Encounters/WildPokemon.cs
+++ Assets/Runtime/Scripts/Engine/Encounters/WildPokemon.cs
@@ -1,8 +1,50 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+#endif
+
 [System.Serializable]
 public class WildPokemon
 {
     public PokemonBase pokemon;
-    public int minLvl;
-    public int maxLvl;
+    [UnityEngine.SerializeField] public WildLevel level;
     public int rate;
 }
+
+[System.Serializable]
+public class WildLevel
+{
+    public int minLvl;
+    public int maxLvl;
+}
+
+#if UNITY_EDITOR
+[
+    CustomPropertyDrawer(typeof(WildLevel), true),
+    CanEditMultipleObjects
+]
+public class WildLevelPropertyDrawer : PropertyDrawer
+{
+    private float buffer = 10;
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        => EditorGUIUtility.singleLineHeight;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var minLevelProperty = property.FindPropertyRelative("minLvl");
+        var maxLevelProperty = property.FindPropertyRelative("maxLvl");
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        position.width = position.width / 2 - (buffer / 2);
+
+        EditorGUI.PropertyField(position, minLevelProperty, true);
+
+        position.x += position.width + buffer;
+        EditorGUI.PropertyField(position, maxLevelProperty, true);
+
+        EditorGUI.EndProperty();
+    }
+}
+#endif
